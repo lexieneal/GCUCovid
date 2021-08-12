ceaa3653cea76cb9fd2ea3554bb9b82c7a0a1980
lexieneal <lexieneal13@gmail.com>
2021-08-12 18:43:25 +0100

    merging previous coursework attempt with new attempt

--- routes/contact.js
+++ routes/contact.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Form = require('../models/form')
 
+
 //Route for contact form page
 router.get('/new', (req, res) => {
     res.render('contact/new', {form: new Form() })
@@ -9,7 +10,18 @@ router.get('/new', (req, res) => {
 
 //Route to submit contact form
 router.post('/', (req, res) => {
-    res.send('Create')
+    const form = new Form({
+        name: req.body.name
+    })
+    form.save((err, newForm) => {
+        if (err) {
+            res.render('contact/new', {
+                errorMessage: 'Error: Please try again'
+            })
+        } else {
+            res.redirect('contact/new')
+        }
+    })
 })
 
 module.exports = router
